66022537eadede4abe4ceebaddad30c34bd33c6e
Bhavesh Banshiwal <97866170+bhaveshbanshiwal@users.noreply.github.com>
2026-06-08 17:39:29 +0530

    servo v3

--- servo_test.py
+++ servo_test.py
@@ -34,7 +34,7 @@ def set_angle(angle):
 try:
     print("Testing SG90 Servo on GPIO 13. Press Ctrl+C to stop.")
 
-    for i in range(10, 360):
+    for i in range(10, 360, 10):
         print(f"Moving to {i}")
         set_angle(i)
         time.sleep(1)
